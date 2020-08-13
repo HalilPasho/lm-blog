904b5fd05d4629e032454bf8f957f5ca5edb86a0
unknown <halil.pasho@moveoalbania.com>
2020-08-13 14:30:40 +0200

    add

---
+++ precache-manifest.9e0d7b7606f74881343283b4e2db16b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965c6a21547d53bea2d8b48116fe17e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd411957f65213039156",
+    "url": "/static/css/main.d48f223f.chunk.css"
+  },
+  {
+    "revision": "4d2b3cee70aaeeec85ab",
+    "url": "/static/js/2.f55e59ef.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f55e59ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd411957f65213039156",
+    "url": "/static/js/main.e770643c.chunk.js"
+  },
+  {
+    "revision": "b145879a3ea2b92ecb6e",
+    "url": "/static/js/runtime-main.9622e2c1.js"
+  }
+]);
